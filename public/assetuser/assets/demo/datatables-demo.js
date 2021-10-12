4c49364c0ba7242ec1c701cf03987f11deddb402
agriapriliando <agripriliando@gmail.com>
2021-10-12 15:26:18 +0700

    fix datatable cant work on hosting

--- public/assetuser/assets/demo/datatables-demo.js
+++ public/assetuser/assets/demo/datatables-demo.js
@@ -1,13 +1,4 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#dataTable').DataTable();
-  $('#dataTablePhoto').DataTable({
-    "columnDefs": [{
-      "width": "5%", 
-      "targets": 0 
-    },{
-      "width": "10%", 
-      "targets": 1 
-    }]
-  });
 });
--- resources/views/components/user/app-layout.blade.php
+++ resources/views/components/user/app-layout.blade.php
@@ -154,8 +154,8 @@
                 </footer>
             </div>
         </div>
-        {{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script> --}}
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> --}}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="{{asset('assetuser/js/scripts.js')}}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
@@ -177,6 +177,16 @@
             alert("Copied the text: " + copyText.value);
             }
             $(document).ready(function(){
+                // js datatable fix on server
+                $('#dataTablePhoto').DataTable({
+                    "columnDefs": [{
+                    "width": "5%", 
+                    "targets": 0 
+                    },{
+                    "width": "10%", 
+                    "targets": 1 
+                    }]
+                });
                 $('body').on('click', '#PhotoDelete', function (event) {
                 event.preventDefault();
                 var id = $(this).data('id');
