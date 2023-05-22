1f0381efe5ed9dda48beaaf967cdb05885be9ae1
agriapriliando <agripriliando@gmail.com>
2023-05-22 14:27:44 +0700

    button pmb home

--- resources/views/components/beritahome.blade.php
+++ resources/views/components/beritahome.blade.php
@@ -1,5 +1,12 @@
 <div class="dayak-bg wow animate__animated animate__fadeInUp animate__faster">
 <div class="container">
+    <div class="row pt-3">
+        <div class="col">
+            <a href="https://iaknpky.ac.id/info/seleksi-mandiri" target="_blank">
+                <img style="border-radius:7px" class="img-fluid mx-auto d-block" src="https://i.ibb.co/9Y7dK7m/button-pmb2023iii.gif" alt="button-pmb2023">
+            </a>
+        </div>
+    </div>
     <div class="row py-3 wow animate__animated animate__fadeInUp animate__rubberBand">
         <div class="col justify-content-xs-center">
             <div class="p-3 text-center">
